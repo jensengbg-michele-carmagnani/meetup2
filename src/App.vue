21edfc2caf6e2c60f57093f07542c5f4216ad983
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-02-17 22:00:07 +0100

    modify heroku.yml and Dockerfile backend

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Casa</router-link> |
       <router-link to="/login">About</router-link>
     </div>
     <router-view />
